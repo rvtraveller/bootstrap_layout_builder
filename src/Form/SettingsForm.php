2445349ebb63f909688b2e4ff64bc668aeed2ffb
Mahmoud Zayed <mahmoudeid2050@gmail.com>
2020-05-27 00:40:18 -0400

    Issue #3134548 by mahmoud-zayed: Add configuration form with option to show or hide the section settings.

---
+++ src/Form/SettingsForm.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Drupal\bootstrap_layout_builder\Form;
+
+use Drupal\Core\Form\ConfigFormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Configure Bootstrap Layout Builder settings.
+ */
+class SettingsForm extends ConfigFormBase {
+
+  /**
+   * Config settings.
+   *
+   * @var string
+   */
+  const SETTINGS = 'bootstrap_layout_builder.settings';
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'bootstrap_layout_builder_admin_settings';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getEditableConfigNames() {
+    return [
+      static::SETTINGS,
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $config = $this->config(static::SETTINGS);
+
+    $form['hide_section_settings'] = [
+      '#type' => 'checkbox',
+      '#title' => $this->t('Hide section settings'),
+      '#default_value' => $config->get('hide_section_settings'),
+    ];
+
+    return parent::buildForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $this->configFactory->getEditable(static::SETTINGS)
+      ->set('hide_section_settings', $form_state->getValue('hide_section_settings'))
+      ->save();
+
+    parent::submitForm($form, $form_state);
+  }
+
+}
--- src/Plugin/Layout/BootstrapLayout.php
+++ src/Plugin/Layout/BootstrapLayout.php
@@ -4,6 +4,9 @@ namespace Drupal\bootstrap_layout_builder\Plugin\Layout;
 
 use Drupal\Core\Layout\LayoutDefault;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * A layout from our bootstrap layout builder.
@@ -13,7 +16,43 @@ use Drupal\Core\Form\FormStateInterface;
  *   deriver = "Drupal\bootstrap_layout_builder\Plugin\Deriver\BootstrapLayoutDeriver"
  * )
  */
-class BootstrapLayout extends LayoutDefault {
+class BootstrapLayout extends LayoutDefault implements ContainerFactoryPluginInterface {
+
+  /**
+   * The config factory.
+   *
+   * @var \Drupal\Core\Config\ConfigFactoryInterface
+   */
+  protected $configFactory;
+
+  /**
+   * Constructs a new class instance.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param array $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $configFactory
+   *   Config factory service.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, ConfigFactoryInterface $configFactory) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->configFactory = $configFactory;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('config.factory'),
+    );
+  }
 
   /**
    * {@inheritdoc}
@@ -72,79 +111,96 @@ class BootstrapLayout extends LayoutDefault {
   }
 
   /**
+   * Helper function to get section settings show/hide status.
+   *
+   * @return bool
+   *   Section settings status.
+   */
+  public function sectionSettingsIsHidden() {
+    $config = $this->configFactory->get('bootstrap_layout_builder.settings');
+    $hide_section_settings = FALSE;
+    if ($config->get('hide_section_settings')) {
+      $hide_section_settings = (bool) $config->get('hide_section_settings');
+    }
+    return $hide_section_settings;
+  }
+
+  /**
    * {@inheritdoc}
    */
   public function buildConfigurationForm(array $form, FormStateInterface $form_state) {
     $form = parent::buildConfigurationForm($form, $form_state);
 
-    $form['has_container'] = [
-      '#type' => 'checkbox',
-      '#title' => $this->t('Add Container'),
-      '#default_value' => (int) !empty($this->configuration['container']) ? TRUE : FALSE,
-    ];
-
-    $container_types = [
-      'container' => $this->t('Container'),
-      'container-fluid' => $this->t('Container fluid'),
-    ];
-
-    $form['container_type'] = [
-      '#type' => 'select',
-      '#title' => $this->t('Container type'),
-      '#options' => $container_types,
-      '#default_value' => !empty($this->configuration['container']) ? $this->configuration['container'] : 'container',
-      '#states' => [
-        'visible' => [
-          ':input[name="layout_settings[has_container]"]' => ['checked' => TRUE],
-        ],
-      ],
-    ];
-
-    $form['has_container_wrapper'] = [
-      '#type' => 'checkbox',
-      '#title' => $this->t('Add Container Wrapper'),
-      '#default_value' => (int) !empty($this->configuration['container_wrapper_classes']) ? TRUE : FALSE,
-      '#states' => [
-        'visible' => [
-          ':input[name="layout_settings[has_container]"]' => ['checked' => TRUE],
-        ],
-      ],
-    ];
-
-    $form['container_wrapper_classes'] = [
-      '#type' => 'textfield',
-      '#title' => $this->t('Container wrapper classes'),
-      '#description' => $this->t('Add classes separated by space. Ex: bg-warning py-5.'),
-      '#default_value' => $this->configuration['container_wrapper_classes'],
-      '#states' => [
-        'visible' => [
-          ':input[name="layout_settings[has_container_wrapper]"]' => ['checked' => TRUE],
-        ],
-      ],
-    ];
-
-    $form['section_classes'] = [
-      '#type' => 'textfield',
-      '#title' => $this->t('Row classes'),
-      '#description' => $this->t('Row has "row" class, you can add more classes separated by space. Ex: no-gutters py-3.'),
-      '#default_value' => $this->configuration['section_classes'],
-    ];
-
-    $form['regions'] = [
-      '#type' => 'details',
-      '#title' => $this->t('Columns Settings'),
-      '#description' => $this->t('Add classes separated by space. Ex: col mb-5 py-3.'),
-      '#open' => TRUE,
-    ];
-
-    foreach ($this->getPluginDefinition()->getRegionNames() as $region_name) {
-      $form['regions'][$region_name . '_classes'] = [
-        '#type' => 'textfield',
-        '#title' => $this->getPluginDefinition()->getRegionLabels()[$region_name] . ' ' . $this->t('classes'),
-        '#default_value' => $this->configuration['regions_classes'][$region_name],
+    // Check if section settings visible.
+    if (!$this->sectionSettingsIsHidden()) {
+      $form['has_container'] = [
+        '#type' => 'checkbox',
+        '#title' => $this->t('Add Container'),
+        '#default_value' => (int) !empty($this->configuration['container']) ? TRUE : FALSE,
       ];
-    }
 
+      $container_types = [
+        'container' => $this->t('Container'),
+        'container-fluid' => $this->t('Container fluid'),
+      ];
+
+      $form['container_type'] = [
+        '#type' => 'select',
+        '#title' => $this->t('Container type'),
+        '#options' => $container_types,
+        '#default_value' => !empty($this->configuration['container']) ? $this->configuration['container'] : 'container',
+        '#states' => [
+          'visible' => [
+            ':input[name="layout_settings[has_container]"]' => ['checked' => TRUE],
+          ],
+        ],
+      ];
+
+      $form['has_container_wrapper'] = [
+        '#type' => 'checkbox',
+        '#title' => $this->t('Add Container Wrapper'),
+        '#default_value' => (int) !empty($this->configuration['container_wrapper_classes']) ? TRUE : FALSE,
+        '#states' => [
+          'visible' => [
+            ':input[name="layout_settings[has_container]"]' => ['checked' => TRUE],
+          ],
+        ],
+      ];
+
+      $form['container_wrapper_classes'] = [
+        '#type' => 'textfield',
+        '#title' => $this->t('Container wrapper classes'),
+        '#description' => $this->t('Add classes separated by space. Ex: bg-warning py-5.'),
+        '#default_value' => $this->configuration['container_wrapper_classes'],
+        '#states' => [
+          'visible' => [
+            ':input[name="layout_settings[has_container_wrapper]"]' => ['checked' => TRUE],
+          ],
+        ],
+      ];
+
+      $form['section_classes'] = [
+        '#type' => 'textfield',
+        '#title' => $this->t('Row classes'),
+        '#description' => $this->t('Row has "row" class, you can add more classes separated by space. Ex: no-gutters py-3.'),
+        '#default_value' => $this->configuration['section_classes'],
+      ];
+
+      $form['regions'] = [
+        '#type' => 'details',
+        '#title' => $this->t('Columns Settings'),
+        '#description' => $this->t('Add classes separated by space. Ex: col mb-5 py-3.'),
+        '#open' => TRUE,
+      ];
+
+      foreach ($this->getPluginDefinition()->getRegionNames() as $region_name) {
+        $form['regions'][$region_name . '_classes'] = [
+          '#type' => 'textfield',
+          '#title' => $this->getPluginDefinition()->getRegionLabels()[$region_name] . ' ' . $this->t('classes'),
+          '#default_value' => $this->configuration['regions_classes'][$region_name],
+        ];
+      }
+    }
     return $form;
   }
 
@@ -153,19 +209,23 @@ class BootstrapLayout extends LayoutDefault {
    */
   public function submitConfigurationForm(array &$form, FormStateInterface $form_state) {
     parent::submitConfigurationForm($form, $form_state);
-    // Container type.
-    $this->configuration['container'] = '';
-    if ($form_state->getValue('has_container')) {
-      $this->configuration['container'] = $form_state->getValue('container_type');
-      // Container wrapper.
-      if ($form_state->getValue('has_container_wrapper')) {
-        $this->configuration['container_wrapper_classes'] = $form_state->getValue('container_wrapper_classes');
-      }
-    }
 
-    $this->configuration['section_classes'] = $form_state->getValue('section_classes');
-    foreach ($this->getPluginDefinition()->getRegionNames() as $region_name) {
-      $this->configuration['regions_classes'][$region_name] = $form_state->getValue('regions')[$region_name . '_classes'];
+    // Check if section settings visible.
+    if (!$this->sectionSettingsIsHidden()) {
+      // Container type.
+      $this->configuration['container'] = '';
+      if ($form_state->getValue('has_container')) {
+        $this->configuration['container'] = $form_state->getValue('container_type');
+        // Container wrapper.
+        if ($form_state->getValue('has_container_wrapper')) {
+          $this->configuration['container_wrapper_classes'] = $form_state->getValue('container_wrapper_classes');
+        }
+      }
+
+      $this->configuration['section_classes'] = $form_state->getValue('section_classes');
+      foreach ($this->getPluginDefinition()->getRegionNames() as $region_name) {
+        $this->configuration['regions_classes'][$region_name] = $form_state->getValue('regions')[$region_name . '_classes'];
+      }
     }
   }
 
